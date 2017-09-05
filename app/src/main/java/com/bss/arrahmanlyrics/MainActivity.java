29eb292fa7935ed8f9da69d4715e26cd744057f9
RK <a.r.ramkumar25@gmail.com>
2017-09-05 19:49:28 +0530

    ini

--- app/src/main/java/com/bss/arrahmanlyrics/MainActivity.java
+++ app/src/main/java/com/bss/arrahmanlyrics/MainActivity.java
@@ -1,7 +1,13 @@
 package com.bss.arrahmanlyrics;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 	}
 }
